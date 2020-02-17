9da2288d601ed3258171cde7902e7bf24c4dc436
iKnowJavaScript <remymartins0398@yahoo.com>
2020-02-17 18:19:30 +0100

    Add component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
-const routes: Routes = [{ path: "", component: HomePageComponent }];
+const routes: Routes = [
+  { path: "", redirectTo: "/index", pathMatch: "full" },
+  { path: "index", component: HomePageComponent },
+  { path: "home", component: LandingPageComponent },
+  { path: "**", redirectTo: "/index" }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
