4bd2f9f3d2ec9cde66cacb4f87f0c63035bf9f79
iKnowJavaScript <remymartins0398@yahoo.com>
2020-02-22 21:25:00 +0100

    Add method that format number to whole number

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-progress-bar',
-  templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.css']
+  selector: "app-progress-bar",
+  templateUrl: "./progress-bar.component.html",
+  styleUrls: ["./progress-bar.component.css"]
 })
 export class ProgressBarComponent implements OnInit {
-@Input()
-width: any;
-@Input()
-state: string;
-  constructor() { }
+  @Input()
+  width: any;
+  @Input()
+  state: string;
+  constructor() {}
 
-  ngOnInit() {
-    
+  ngOnInit() {}
+
+  toFixed(number: string, index = 2) {
+    return Number(number).toFixed(index);
   }
-
 }
