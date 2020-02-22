7d693da570deb26479dfa4c0d9c247d1718d36af
iKnowJavaScript <remymartins0398@yahoo.com>
2020-02-22 13:02:57 +0100

    Add twitter card

---
+++ src/app/twitter-tile/twitter-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-twitter-tile',
+  templateUrl: './twitter-tile.component.html',
+  styleUrls: ['./twitter-tile.component.css']
+})
+export class TwitterTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
