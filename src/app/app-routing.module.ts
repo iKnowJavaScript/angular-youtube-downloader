9aaf3d8561060012d411a1f44e53aa5d494088c9
iKnowJavaScript <remymartins0398@yahoo.com>
2020-02-22 21:29:46 +0100

    Update module on component removal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { HomePageComponent } from "./home-page/home-page.component";
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/index", pathMatch: "full" },
-  { path: "index", component: HomePageComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: LandingPageComponent },
-  { path: "**", redirectTo: "/index" }
+  { path: "**", redirectTo: "/home" }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
